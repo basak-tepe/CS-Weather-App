11efca276d10120f2ebdf4a385251149d4c07a9d
basak-tepe <basaktepe2020@gmail.com>
2023-08-27 21:58:12 +0300

    Made the API call

--- CSWeatherApp/Form1.cs
+++ CSWeatherApp/Form1.cs
@@ -1,7 +1,9 @@
 using Newtonsoft.Json;
 using System.Net;
-namespace CSWeatherApp
-{
+using System.Security.Policy;
+
+namespace CSWeatherApp { 
+
     public partial class condition : Form
     {
         public condition()
@@ -49,9 +51,22 @@ namespace CSWeatherApp
 
         }
 
+
+        string APIKey = "16c18bf01de8fa1e5f16f7c3d67d6f5f";
         void getWeather()
         {
             //we will de-seriealize JSON into our weatherInformation class
+
+            //form a web client
+
+            using (WebClient web = new WebClient()) {
+
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}",TBCity.Text, APIKey);
+                var json = web.DownloadString(url);
+
+                //deserialize
+                WeatherInformation.root info = JsonConvert.DeserializeObject<WeatherInformation.root>(json);
+            }
 
         }
     }
